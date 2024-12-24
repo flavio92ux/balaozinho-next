66fcea3bfb31b8d951d134903bf95a62614ba321
Flavio F <flavio.win@gmail.com>
2024-12-23 22:53:14 -0300

    Adicionando Header e Footer

--- app/flavio/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Flavio() {
-  return (
-    <h1>Flavio</h1>
-  );
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 import "./globals.css";
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
