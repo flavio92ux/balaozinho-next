50fb948c96299a75cdccfbc7653aa60d4e0a53f0
Flavio F <flavio.win@gmail.com>
2024-12-26 11:09:07 -0300

    Adicionando imagem de fundo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <h1></h1>
+    <div
+      style={{
+        backgroundImage: "url(/sky-background.png)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="w-full h-[654px]"
+    >
+
+    </div>
   );
 }
