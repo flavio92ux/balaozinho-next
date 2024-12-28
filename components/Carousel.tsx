9d3448c1fc710f1683a30dd3b24ae364c6bf79b0
Flavio F <flavio.win@gmail.com>
2024-12-28 12:24:26 -0300

    Ajusta css

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,23 +1,39 @@
 "use client";
 
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
-// Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
+import "./custom-carousel.css";
 import Image from "next/image";
 
 export default function Carousel() {
   return (
-    <Swiper spaceBetween={50} slidesPerView={1}>
-      <SwiperSlide className='flex justify-center items-center h-full'>
-        <div className='flex justify-center items-center w-full h-full'>
-          <Image src="/Component 1.png" alt="Banner" width={1200} height={500} />
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      modules={[Navigation]}
+    >
+      <SwiperSlide className="flex justify-center items-center h-full">
+        <div className="flex justify-center items-center w-full h-full">
+          <Image
+            src="/Component 1.png"
+            alt="Banner"
+            width={1200}
+            height={500}
+          />
         </div>
       </SwiperSlide>
-      <SwiperSlide className='flex justify-center items-center h-full'>
-        <div className='flex justify-center items-center w-full h-full'>
-          <Image src="/Component 1.png" alt="Banner" width={1200} height={500} />
+      <SwiperSlide className="flex justify-center items-center h-full">
+        <div className="flex justify-center items-center w-full h-full">
+          <Image
+            src="/Component 1.png"
+            alt="Banner"
+            width={1200}
+            height={500}
+          />
         </div>
       </SwiperSlide>
     </Swiper>
